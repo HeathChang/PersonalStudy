ae5da7319e98a363cdfa081b79633e57fd813d18
HeathChang <jannhyunsoo@gmail.com>
2021-06-19 13:05:17 +0900

    210619_heroku_error_fixed

--- fullstackReact_node/server/index.js
+++ fullstackReact_node/server/index.js
@@ -1,8 +1,14 @@
 const express = require('express'); //common 
 //import express from 'express'; //react side
+
 const app = express();
 app.get('/',(req,res)=>{
-  res.send({hi:"there"});
+  res.send({bye: 'buddy'});
 })
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+//http://localhost:5000/
+
+//https://salty-savannah-38001.herokuapp.com/ | https://git.heroku.com/salty-savannah-38001.git
