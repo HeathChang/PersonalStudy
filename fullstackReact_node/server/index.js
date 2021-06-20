1d104cbe651c5e951973f858595b19962bd76fe7
HeathChang <jannhyunsoo@gmail.com>
2021-06-20 23:43:43 +0900

    210620_gitCheck

--- fullstackReact_node/server/index.js
+++ fullstackReact_node/server/index.js
@@ -4,8 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-//client ID: 70049040011-0480ragjr50aghod7tvm9on0d64c2fih.apps.googleusercontent.com
-//client secret: SJuAoBC2zU7dkpOOMZGYAiNU
+
+
 passport.use(new GoogleStrategy()); //create new instance
 
 const PORT = process.env.PORT || 5000;
