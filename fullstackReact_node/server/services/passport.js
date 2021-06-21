6719eba87dee63eecac9f3a952dfd0c49046baa4
HeathChang <jannhyunsoo@gmail.com>
2021-06-21 17:30:20 +0900

    210621_mongoDB_untilDeserializeUser_46

--- fullstackReact_node/server/services/passport.js
+++ fullstackReact_node/server/services/passport.js
@@ -7,11 +7,35 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users'); //fetching
 
+//profiling(Encoding User): mangooseModel → id
+passport.serializeUser((user,done)=>{ //pulled out from DB(findOne)
+  done(null,user.id); //callback (error Obj), identifying User (not profile.Id) → id assigned by mangoose
+});
+
+//deserialize: user id → mangoose  
+passport.deserializeUser((id, done)=>{
+  User.findById(id).then(user=>{
+    done(null,user);
+  })
+})
+
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback' //route user will be sent to
 },(accessToken,refreshToken,profile,done)=>{
-  new User({googleId: profile.id}).save()
+  User.findOne({googleId:profile.id}) //returns promise
+  .then((existingUser)=> {
+    if(existingUser){//already have a recode with the give profile ID
+      done(null,existingUser);
+    }else{ // don't have a user record with the given profileID
+
+      new User({googleId: profile.id})
+        .save()
+        .then(user=> done(null,user))
+    }
+  })
+  
   
 })); //create new instance
