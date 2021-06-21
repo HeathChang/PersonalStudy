12780f90368713fd9a922446905426a5d5cde1f1
HeathChang <jannhyunsoo@gmail.com>
2021-06-21 15:39:58 +0900

    210621_MongoDB_savingModelInstances

--- fullstackReact_node/server/services/passport.js
+++ fullstackReact_node/server/services/passport.js
@@ -3,18 +3,15 @@
 const passport =require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users'); //fetching
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback' //route user will be sent to
 },(accessToken,refreshToken,profile,done)=>{
-  console.log("accessToken: ",accessToken);
-  //accessToken:
-  console.log("refreshToken: ",refreshToken);
-  //refreshToken:
-  console.log(",profile: ",profile);
-  //profile:
-  console.log("done: ",done);
-  //done:
+  new User({googleId: profile.id}).save()
+  
 })); //create new instance
