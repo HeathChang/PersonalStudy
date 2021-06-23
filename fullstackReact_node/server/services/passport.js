92f2f5998f29886072cfc82ce5a7b2ea279c6878
HeathChang <jannhyunsoo@gmail.com>
2021-06-23 23:10:27 +0900

    210623_ClientReact

--- fullstackReact_node/server/services/passport.js
+++ fullstackReact_node/server/services/passport.js
@@ -1,6 +1,6 @@
 //Helper modules and business logic:
 
-const passport =require('passport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
@@ -8,35 +8,30 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users'); //fetching
 
 //profiling(Encoding User): mangooseModel → id
-passport.serializeUser((user,done)=>{ //pulled out from DB(findOne)
-  done(null,user.id); //callback (error Obj), identifying User (not profile.Id) → id assigned by mangoose
+passport.serializeUser((user, done) => { //pulled out from DB(findOne)
+  done(null, user.id); //callback (error Obj), identifying User (not profile.Id) → id assigned by mangoose
 });
 
 //deserialize: user id → mangoose  
-passport.deserializeUser((id, done)=>{
-  User.findById(id).then(user=>{
-    done(null,user);
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
   })
 })
-
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback', //route user will be sent to
   proxy: true
-},(accessToken,refreshToken,profile,done)=>{
-  User.findOne({googleId:profile.id}) //returns promise
-  .then((existingUser)=> {
-    if(existingUser){//already have a recode with the give profile ID
-      done(null,existingUser);
-    }else{ // don't have a user record with the given profileID
-
-      new User({googleId: profile.id})
-        .save()
-        .then(user=> done(null,user))
-    }
-  })
-  
-  
-})); //create new instance
+},
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id }) //returns promise
+    if (existingUser) {//already have a recode with the give profile ID
+      return done(null, existingUser);
+    }  
+      // don't have a user record with the given profileID
+      const user=await new User({ googleId: profile.id }).save();
+      done(null,user)
+    
+  })); //create new instance
