1b21f617c9b2111e43fb14a1f7c8f211ee0129c9
HeathChang <jannhyunsoo@gmail.com>
2021-06-24 15:32:33 +0900

    210624_intro2React

---
+++ react_course/react-basic/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> //APP의 내용을
+  </React.StrictMode>,
+  document.getElementById('root') //어디에다 보여줄것인지 (id가 root인 아이)
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
